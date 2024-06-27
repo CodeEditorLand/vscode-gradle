a0b23bf8f2de4194c874ade346d8880d4da0418a
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-27 09:25:04 +0300

    squash!

--- gradle-server/src/main/java/com/github/badsyntax/gradle/GradleProjectConnectionType.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-package com.github.badsyntax.gradle;
-
-public enum GradleProjectConnectionType {
-	WRAPPER, SPECIFICVERSION, LOCALINSTALLATION,
-}
