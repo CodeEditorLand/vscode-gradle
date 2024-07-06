38b9dbd4b72569948b8006f24ca664a6474427ec
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-06 12:43:10 +0300

    squash!

--- extension/src/views/recentTasks/RecentTasksRootProjectTreeItem.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from "vscode";
-import { TreeItemWithTasksOrGroups } from "..";
-
-export class RecentTasksRootProjectTreeItem extends TreeItemWithTasksOrGroups {
-    public readonly iconPath = vscode.ThemeIcon.Folder;
-}
